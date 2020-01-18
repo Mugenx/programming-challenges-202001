6aab27d9d6d56ba9c4e99b5da2aa0b5df3ff7509
Mugenx <sahawu.mail@gmail.com>
2020-01-18 13:56:58 -0500

    [palindrome] update

--- Challenge 2 - Palindrome/palindrome.js
+++ Challenge 2 - Palindrome/palindrome.js
@@ -1,26 +1,54 @@
+// digit of the max & min number, default to 3
+const digit = process.argv[2] || 3;
+
+/**
+ * 
+ * @description Logs the result
+ * @param {Number} palindrom The largest palindrome
+ * @param {Number} num factor of the palindrome
+ * 
+ */
+function logResult(palindrom, num) {
+    const result = `${palindrom} = ${num} x ${palindrom / num}`;
+    console.log(`The largest palindrome made from the product of two ${digit}-digit numbers is: ${result}`);
+}
+
+/**
+ * 
+ * @param {Number} num number as fisrt half of the Palindrome
+ * @returns Palindrome number
+ * 
+ */
 function makePalindrome(num) {
     const palindrom = num + ('' + num).split('').reverse().join('')
     return Number(palindrom);
 }
 
+/**
+ * 
+ *      Find the largest palindrome made from the product of numbers
+ * 
+ */
 const getLargiestPalindrome = () => {
-	const max = 999, min = 99;
+    const max = Math.pow(10, digit) - 1;
+    const min = Math.pow(10, digit - 1) - 1;
+
 	let half = Math.floor((max * min) / 100);
 	let stop = false;
 	let palindrom, num;
 	while (!stop) {
-		palindrom = makePalindrome(half);
+        palindrom = makePalindrome(half);
 		for (i = max; i > min; i--) {
-			if (palindrom / i > max || i * i < palindrom) break;
+            if (palindrom / i > max || i * i < palindrom) break;
 			if (palindrom % i === 0) {
-				stop = !stop;
+                stop = !stop;
 				num = i;
 				break;
 			}
 		}
 		half--;
     }
-	console.log(palindrom, '=', num, 'x', palindrom / num);
+    logResult(palindrom, num)
 };
 
 getLargiestPalindrome();
